a20b5b954c1c7cc6509911a8535844e82677c582
solo <solohuckstep@gmail.com>
2022-10-17 00:52:57 -0400

    js was cleaned up, added a for loop, comments, and better functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,56 +1,57 @@
-// gets the current date using moment.js and formats it as "Monday, October (current date)"
+// getting todays current date 
 var currentDate = moment().format("dddd, MMMM Do");
-// sets the current date to the #currentDay id in the HTML
 $("#currentDay").html(currentDate);
+// used to get the current time 
+var currentTime = moment().hour();
+// all the numbers that represent 9am - 5pm
+hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
-$(function () {
-    // click function for the .saveBtn class
-    // seeing if the .saveBtn class is clicked (on) = onclick
-    $(".saveBtn").on("click", function () {
-        // text is equal to the text in the .description class
-        var text = $(this).siblings(".description").val();
-        // time is equal to the id  ex. (#hour10)
-        var time = $(this).parent().attr("id");
-        // sets the local storage of the text (".description") and what time block its set in
-        localStorage.setItem(time, text);
-    })
-    // a fuction to keep track of the current time and to choose the correct color for past, present, and future.
-    function timeKeeper() {
-        // uses moment.js to find the current time
-        var currentTime = moment().hour();
-        // a function for each .time-block class
-        $(".time-block").each(function () {
-            // this timeBlock variable is getting the number from the ids ex. #hour10 
-            var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
-            // if the number from the id is less than the current time, add the past color code and remove the other color classes
-            if (timeBlock < currentTime) {
-                $(this).removeClass("future");
-                $(this).removeClass("present");
-                $(this).addClass("past");
-                // if the number from the id is equall to the current time, add the present color code and remove the other color classes
-            } else if (timeBlock === currentTime) {
-                $(this).removeClass("past");
-                $(this).removeClass("future");
-                $(this).addClass("present");
-                // if its neither the past or the present, add the future color code and remove the other color classes
-            } else {
-                $(this).removeClass("present");
-                $(this).removeClass("past");
-                $(this).addClass("future");
-            }
-        })
-    }
-    // gets the saved info from local storage
-    $("#hour8 .description").val(localStorage.getItem("hour8"));
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
-    // calls the timeKeeper function
-    timeKeeper();
+var container = $('.container');
+
+for (let i = 0; i < hours.length; i++) {
+    var outsideDiv = document.createElement('div')
+    outsideDiv.classList.add('row', 'time-block')
+    // had to leave this weird space so that the time layout would look correct
+    outsideDiv.innerHTML =`<div class="col-md-1 hour">
+    
+${getHourLabel(hours[i])} 
+    </div>
+    <textarea class="col-md-10 description ${getTimeColor(hours[i])}"> ${localStorage.getItem(hours[i]) ||""}
+    </textarea><button class="btn saveBtn col-md-1" data-hour=${hours[i]}><i class="fas fa-save"></i></button>`
+    container.append(outsideDiv)
+}
+// click funtion to see if the .btn is clicked 
+$(".btn").on("click", function (event) {
+    var hour = event.currentTarget.dataset.hour;
+    var text = $(this).siblings(".description").val();
+    localStorage.setItem(hour, text);
 })
+// function to set the correct color 
+function getTimeColor(hour){
+    if (hour < currentTime) return "past"
+    else if (hour == currentTime) return "present"
+    else return "future"
+}
+// gives the 9-17 numbers the correct label
+function getHourLabel(hour){
+    switch (hour) {
+        case 9:
+            return "9 AM"
+        case 10:
+            return "10 AM"
+        case 11:
+            return "11 AM"
+        case 12:
+            return "12 PM"
+        case 13:
+            return "1 PM"
+        case 14:
+            return "2 PM"
+        case 15:
+            return "3 PM"
+        case 16:
+            return "4 PM"
+        case 17:
+            return "5 PM"
+    }
+}
